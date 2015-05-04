145cc93df5d734e614b2dedd2cc22a00747d364d
spiros papanikos <spiro2k6@gmail.com>
2015-05-04 18:44:55 -0400

    added browse and plant_info.txt

--- gen/net/andoidbootcamp/gardenapp/R.java
+++ gen/net/andoidbootcamp/gardenapp/R.java
@@ -25,20 +25,28 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080003;
+        public static final int button1=0x7f080002;
+        public static final int plantDetails=0x7f080001;
+        public static final int plantImage=0x7f080000;
+        public static final int products=0x7f080005;
+        public static final int search=0x7f080004;
     }
     public static final class layout {
-        public static final int activity_my_garden=0x7f030000;
-        public static final int activity_plant=0x7f030001;
+        public static final int activity_browse=0x7f030000;
+        public static final int activity_my_garden=0x7f030001;
+        public static final int activity_plant=0x7f030002;
     }
     public static final class menu {
-        public static final int my_garden=0x7f070000;
-        public static final int plant=0x7f070001;
+        public static final int browse=0x7f070000;
+        public static final int my_garden=0x7f070001;
+        public static final int plant=0x7f070002;
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050001;
+        public static final int title_activity_browse=0x7f050004;
         public static final int title_activity_plant=0x7f050003;
     }
     public static final class style {
